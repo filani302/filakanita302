23a8a12d2d931a8ead38f12bc68e031544051842
filani302 <filani302@hengersor.hu>
2024-09-23 10:28:05 +0200

    kész

---
+++ ingatla/resources/views/bemutatkozas.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bemutatkozás</title>
+</head>
+<body>
+    <h1>Ezen az oldalon található a Bemutatkozást.</h1>
+</body>
+</html>
